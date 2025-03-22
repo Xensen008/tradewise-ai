93da081c8103c0099bd511a28bf28fb51c6bd887
Arjun Bhandari <arjun12345bhandari@gmail.com>
2025-03-22 13:08:11 +0530

    Dashboard

---
+++ src/components/App-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Calculator, Calendar, Home,  MessageCircle, Search, } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Stock Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Portfolio",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "AI Advisory",
+    url: "#",
+    icon: MessageCircle,
+  },
+  {
+    title: "Tax Calculator",
+    url: "#",
+    icon: Calculator,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>TradeWise AI</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
